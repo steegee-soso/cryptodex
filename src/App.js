838e32b4f8e07c534900bd5491e77f27eb3fcb41
Christopher Amanor <kriskit7@gmail.com>
2021-02-17 23:36:37 +0000

    Setup project to make calls to receive data from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cryptos: [],
+      news: []
+    }
+  }
+
+
+  componentDidMount() {
+    const imageUrlPrefix = 'https://www.cryptocompare.com/';
+    const coinUrl = 'https://min-api.cryptocompare.com/data/blockchain/mining/calculator?fsyms=BTC,ETH,ZEC,REP,DASH,BCH,LTC,XMR,XRP,TRX,ADA,USDT,ETC,XTZ&tsyms=USD,EUR,GHS';
+    const newsUrl = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN';
+
+    axios.get(coinUrl)
+      .then(result => {
+        const cryptos = result.data.Data;
+        console.log('cryptos', cryptos);
+        this.setState({ cryptos: cryptos });
+    });
+
+    axios.get(newsUrl)
+      .then(result => {
+        const news = result.data;
+        console.log('news', news);
+        this.setState({
+          news: news
+        })
+      });
+  }
+
+
+  render () {
+    return (
+      <div className="App">
+        {Object.entries(this.state.cryptos).map(
+          (crypto, index) => (
+            <div className="crypto-container" key={index}>
+              <span>{crypto[1].CoinInfo.FullName}</span>
+              <span>{crypto[1].CoinInfo.AssetLaunchDate}</span>
+              <span>{crypto[1].CoinInfo.Name}</span>
+              <span>{crypto[1].CoinInfo.ImageUrl}</span>
+              <span>{crypto[1].CoinInfo.Url}</span>
+              <span>{crypto[1].CoinInfo.TotalCoinsMined}</span>
+              <span>{crypto[1].Price.USD}</span>
+              <span>{crypto[1].Price.EUR}</span>
+              <span>{crypto[1].Price.GHS}</span>
+            </div>
+          ),
+          // console.log('cr', crypto[1].CoinInfo.AssetLaunchDate);
+        )}
+      {/* </div>
+
+      <div className="News"> */}
+        {Object.entries(this.state.news).map(
+          (item, index) => (
+            <div className="crypto-container" key={index}>
+              <span>{item[1].CoinInfo.FullName}</span>
+              <span>{item[1].CoinInfo.AssetLaunchDate}</span>
+              <span>{item[1].CoinInfo.Name}</span>
+              <span>{item[1].CoinInfo.ImageUrl}</span>
+              <span>{item[1].CoinInfo.Url}</span>
+              <span>{item[1].CoinInfo.TotalCoinsMined}</span>
+              <span>{item[1].Price.USD}</span>
+              <span>{item[1].Price.EUR}</span>
+              <span>{item[1].Price.GHS}</span>
+            </div>
+          ),
+          // console.log('cr', crypto[1].CoinInfo.AssetLaunchDate);
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
